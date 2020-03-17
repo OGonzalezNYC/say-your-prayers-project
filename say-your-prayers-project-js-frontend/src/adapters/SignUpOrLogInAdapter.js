118115a6d83589e6cc9aa86e08f091d2338e8dc7
github username <github email address>
2020-03-17 14:21:37 -0400

    Am now able to create a user and login a user through the UI.

--- say-your-prayers-project-js-frontend/src/adapters/SignUpOrLogInAdapter.js
+++ say-your-prayers-project-js-frontend/src/adapters/SignUpOrLogInAdapter.js
@@ -1,38 +1,9 @@
 
 class SignUpOrLogInAdapter {
+
   constructor() {
-    // baseUrl = 'http://localhost:3000/api/v1/'
-    // this.bulkOfFetchCall = `, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Accept: 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     user: {`
+
   }
-
-  // defineInputs() {
-    // this.
-    newUsername = document.getElementById("new-user-username").value;
-    // this.
-    newPassword = document.getElementById("new-user-password").value;
-    // this.
-    returningUsername = document.getElementById("login-username").value;
-    // this.
-    returningPassword = document.getElementById("login-password").value;
-  // }
-
-  // this.baseUrl = 'http://localhost:3000/api/v1/'
-  // this.bulkOfFetchCall = `, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Accept: 'application/json'
-  //   },
-  //   body: JSON.stringify({
-  //     user: {`
-  // }
 
   submitNewUserInfo(e) {
     e.preventDefault()
@@ -41,50 +12,41 @@ class SignUpOrLogInAdapter {
     const password = document.querySelector("#new-user-password").value
     // fetch(this.baseUrl + 'users' + this.bulkOfFetchCall + ' username: ' + this.newUsername + ', password: ' + this.newPassword + '} }) }')
     fetch('http://localhost:3000/api/v1/users', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  },
-  body: JSON.stringify({
-    user: {
-      username: `${username}`,
-      password: `${password}`
-    }
-  })
-})
-  .then(r => r.json())
-  .then(console.log)
-    // .then(r => r.json())
-    // .then(console.log)
-    //.then
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: `${username}`,
+          password: `${password}`
+        }
+      })
+    })
+    .then(r => r.json())
+    .then(console.log)
   }
 
-
-  submitReturningUserInfo() {
-    // fetch(this.baseUrl + 'login' + this.bulkOfFetchCall + ' username: ' + this.returningUsername + ', password: ' + this.returningPassword + '} }) }')
-    // .then(r => r.json())
-    // .then(console.log)
-
-    //.then
-
-    fetch('http://localhost:3000/api/v1/users', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  },
-  body: JSON.stringify({
-    user: {
-      username: "Lucky",
-      password: "LuckyPassword"
-    }
-  })
-})
-  .then(r => r.json())
-  .then(console.log)
-
-
-
+  submitReturningUserInfo(e) {
+    e.preventDefault()
+    const username = document.querySelector("#login-username").value
+    const password = document.querySelector("#login-password").value
+    fetch('http://localhost:3000/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: `${username}`,
+          password: `${password}`
+        }
+      })
+    })
+    .then(r => r.json())
+    .then(console.log)
   }
+
 }
--- say-your-prayers-project-js-frontend/src/components/signUpOrLogIn.js
+++ say-your-prayers-project-js-frontend/src/components/signUpOrLogIn.js
@@ -1,14 +1,17 @@
 class SignUpOrLogIn {
+
   constructor() {
    // this.signUpOrLogIns = [];
    this.adapter = new SignUpOrLogInAdapter();
    this.defineButtons();
    this.addEventListeners();
   }
+
   defineButtons() {
     this.newUserSubmit = document.getElementById("new-user-submit")
     this.returningUserSubmit = document.getElementById("login-submit")
   }
+  
   addEventListeners() {
     this.newUserSubmit.addEventListener("click", this.adapter.submitNewUserInfo);
     this.returningUserSubmit.addEventListener("click", this.adapter.submitReturningUserInfo);
